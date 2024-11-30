bac341227985521d83eb48ae79eb68304086a9bf
Ranime Shehata <ranimeshehata@gmail.com>
2024-12-01 01:37:04 +0200

    feat: email and password validations for login + userContext + dummy call for endpoints

--- frontend-app/voterra/src/App.jsx
+++ frontend-app/voterra/src/App.jsx
@@ -1,22 +1,25 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './style.css'
 import Login from './pages/Login'
+import HomePage from './pages/HomePage'
 import IntroPage from './pages/IntroPage'
+import { UserProvider } from './context/UserContext'
 
 function App() {
 
   return (
-    <>
+    <UserProvider>
       <div>
         <Router>
           <Routes>
             <Route path="/" element={ <IntroPage />} />
             <Route path="/login" element={ <Login />} />
+            <Route path="/homepage" element={ <HomePage />} />
           </Routes>
         </Router>
       </div>
       
-    </>
+    </UserProvider>
   )
 }
 
--- frontend-app/voterra/src/components/LoginForm.jsx
+++ frontend-app/voterra/src/components/LoginForm.jsx
@@ -1,15 +1,125 @@
-import { useState } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { Col} from 'react-bootstrap';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { axios } from 'react-axios';
+import { UserContext } from '../context/UserContext';
+import { useNavigate } from 'react-router-dom';
 import mailIcon from '../assets/mail-icon.svg';
 import eyeOffIcon from '../assets/eye-off-icon.svg';
 import eyeIcon from '../assets/eye-icon.svg';
 
 function LoginForm() {
+    const validationErrors={};
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [loginError, setLoginError] = useState('');
+    const [errors, setErrors] = useState({});
     const [showPassword, setShowPassword] = useState(false);
+
+    const navigate = useNavigate();
+    const { setUser, login, setIsAuthenticated } = useContext(UserContext);
 
     const togglePasswordVisibility = () => {
         setShowPassword(!showPassword);
     };
+
+    useEffect(() => {
+        const handleStorageChange = event => {
+          if (event.key === 'token' && event.newValue) {
+            toast.info('Another user is already logged in!');
+            navigate('/homepage');
+          }
+        };
+    
+        window.addEventListener('storage', handleStorageChange);
+    
+        return () => {
+          window.removeEventListener('storage', handleStorageChange);
+        };
+      }, [navigate, setIsAuthenticated]);
+
+    const validateEmail = inputEmail => {
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(String(inputEmail).toLowerCase());
+      };
+    
+      const validatePassword = inputPassword => {
+        const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        return re.test(inputPassword);
+      };
+    
+      const handleValidation = () => {
+        setLoginError('');
+        let valid = true;
+        if (!email) {
+          validationErrors.email = 'This field cannot be empty.';
+          valid = false;
+        } else if (!validateEmail(email)) {
+          validationErrors.email = 'Invalid Email Format.';
+          valid = false;
+        }
+    
+        if (!password) {
+          validationErrors.password = 'This field cannot be empty.';
+          valid = false;
+        } else if (valid && !validatePassword(password)) {
+          validationErrors.password = 'Invalid Email or Password.';
+        }
+        setErrors(validationErrors);
+        if (Object.keys(validationErrors).length === 0) {
+          return true;
+        }
+        return false;
+      };
+
+      const resetLoginForm = () => {
+        setEmail('');
+        setPassword('');
+        setLoginError('');
+      };
+
+    const callBackend = () => {
+        axios
+          .post('http://localhost:8080/users/login', {
+            email,
+            password,
+          })
+          .then(response => {
+            const token = response.headers.authorization;
+            if (token) {
+              localStorage.setItem('token', token);
+              localStorage.setItem('user', response.data);
+            }
+            const user = response.data;
+            setUser(user);
+            login();
+            navigate('/homepage');
+            resetLoginForm();
+          })
+          .catch(err => {
+            if (!err.response) {
+                setLoginError('No server response.');
+              } else if (err.response.status === 401||err.response.status === 422) { // wrong email,pwd
+                setLoginError('Invalid Email or Password.');
+              } else {
+                setLoginError('Login failed. Please try again.');
+              }
+          });
+    }
+
+      const handleSubmit = e => {
+        e.preventDefault();
+        if (localStorage.getItem('token')) {
+          toast.info('Another user is already logged in!');
+          return;
+        }
+        const isValid=handleValidation();
+        if (isValid) {
+          callBackend();
+        }
+      };
+    
 
     return (
       <div className="flex justify-center items-center min-h-screen form-container">
@@ -28,10 +138,12 @@ function LoginForm() {
                 </label>
                 <div className="flex items-center border rounded-lg">
                     <input
-                        className="email-input"
+                        className={`email-input ${(errors.email || errors.password || loginError) ? 'errorBorder' : ''}`}
                         id="email"
                         type="text"
                         placeholder="Enter email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
                     />
                     <img 
                         src={mailIcon} 
@@ -39,6 +151,11 @@ function LoginForm() {
                         className="h-5 w-5 cursor-pointer mx-2" 
                     />
                 </div>
+                {errors.email && (
+          <p className="errorMsg">
+            {errors.email}
+          </p>
+        )}
             </Col>
 
             <Col className="relative mb-5 inputField">
@@ -47,10 +164,13 @@ function LoginForm() {
                 </label>
                 <div className="flex items-center border rounded-lg">
                     <input
-                        className="password-input"
+                        className={`password-input ${(!errors.email || (errors.email && errors.password)) && (errors.email || errors.password || loginError) ? 'errorBorder' : ''}`}
                         id="password"
                         type={showPassword ? 'text' : 'password'}
                         placeholder="Enter password"
+                        onChange={e => setPassword(e.target.value)}
+                        onCopy={e => e.preventDefault()}
+                        onCut={e => e.preventDefault()}
                     />
                     <img
                         src={showPassword ? eyeIcon : eyeOffIcon}
@@ -64,9 +184,21 @@ function LoginForm() {
                         Forgot Password?
                     </a>
                 </div>
+                {errors.password && (
+          <p className="errorMsg">
+            {errors.password}
+          </p>
+        )}
+        {loginError && (
+          <p className="errorMsg">{loginError}</p>
+        )}
             </Col>
             <Col className="mb-4">
-                <button className="loginButton" type="submit">
+                <button
+                    className="loginButton"
+                    type="submit"
+                    onClick={ handleSubmit }
+                >
                     Login
                 </button>
             </Col>
