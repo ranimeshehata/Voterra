9a4ac6f3e73df89b8c89b50064f7075d4785ea1f
Ranime Shehata <ranimeshehata@gmail.com>
2024-12-02 15:44:35 +0200

    fix: rendering react components on routing (using lazy()), better for scalability in future

--- frontend-app/voterra/src/App.jsx
+++ frontend-app/voterra/src/App.jsx
@@ -1,14 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './style.css';
-import Login from './pages/Login';
-import HomePage from './pages/HomePage';
-import IntroPage from './pages/IntroPage';
 import React, { Suspense } from 'react';
 import './style.css';
 import useAuth from './hooks/useAuth';
-// const Login = React.lazy(() => import('./pages/Login'));
-// const IntroPage = React.lazy(() => import('./pages/IntroPage'));
+const Login = React.lazy(() => import('./pages/Login'));
+const IntroPage = React.lazy(() => import('./pages/IntroPage'));
 const SignUp = React.lazy(() => import('./pages/SignUp'));
+const HomePage = React.lazy(() => import('./pages/HomePage'));
 
 function App() {
   useAuth(); // Initialize authentication state
