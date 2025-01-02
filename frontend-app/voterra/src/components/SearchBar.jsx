92846422c29b3aa6461f3c2d0919aec2a2eeb3a2
ranimeshehata <ranimeshehata@gmail.com>
2025-01-02 10:16:42 +0200

    fix: update data source configuration and clean up unused variables in components

--- frontend-app/voterra/src/components/SearchBar.jsx
+++ frontend-app/voterra/src/components/SearchBar.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
 import { useRecoilState } from "recoil";
 import { currFilter, currSearch } from "../recoil/atoms";
